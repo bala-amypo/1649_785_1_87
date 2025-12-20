e1c7743a1b47f91f75edb0e43d7d179819d1fc6a
bala-amypo <bala@amypo.in>
2025-12-20 12:52:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/ActivityCategory.java
+++ src/main/java/com/example/demo/entity/ActivityCategory.java
@@ -1,1 +1,20 @@
-package com.example.demo.
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+public class ActivityCategory{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String categoryName;
+    
+}
