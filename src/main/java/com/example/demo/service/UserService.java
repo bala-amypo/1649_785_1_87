390df69f8451078400d19d1069ee75f33cfadfd3
bala-amypo <bala@amypo.in>
2025-12-20 16:40:28 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,7 +3,7 @@ import com.example.demo.entity.User;
 import java.util.*;
 public interface UserService{
     User registerUser(User stu);
-    List<User> getAllUser();
+    List<User> getAllUsers();
     User getUser(Long id);
     User getByEmail(String email);
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -5,37 +5,26 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Override;
 @Service
 public class UserServiceImpl implements UserService{
      @Autowired UserRepository student;
      
           @Override
-          public User postData(User stu){
+          public User registerUser(User stu){
                return student.save(stu);
           }
           @Override
-          public List<User> getData(){
+          public List<User> getAllUsers(){
                return student.findAll();
           }
+          
           @Override
-          public String deleteData(int id){
-               student.deleteById(id);
-               return "Delete Successfully!";
-          }
-          @Override
-          public User findData(int id){
+          public User getUser(Long id){
                return student.findById(id).orElse(null);
           }
           @Override
-          public User updateData(int id,User s){
-               if(student.existsById(id)){
-                    s.setId(id);
-                    return student.save(s);
-               }
-               else{
-                    return null;
-               }
+          public User getByEmail(String email){
+               return student.findByEmail().orElse(null);
           }
 }
 
