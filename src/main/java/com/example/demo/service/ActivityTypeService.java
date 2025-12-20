27947b48bbda8c98fb5ecc1185223fcbd145a8d6
bala-amypo <bala@amypo.in>
2025-12-20 16:49:28 +0000

    User commit

--- src/main/java/com/example/demo/service/ActivityTypeService.java
+++ src/main/java/com/example/demo/service/ActivityTypeService.java
@@ -2,8 +2,7 @@ package com.example.demo.service;
 import com.example.demo.entity.ActivityType;
 import java.util.*;
 public interface ActivityTypeService{
-    ActivityType registerActivityType(ActivityType stu);
-    List<ActivityType> getAllActivityTypes();
-    ActivityType getActivityType(Long id);
-    ActivityType getByEmail(String email);
+    ActivityType createType(ActivityType stu);
+    List<ActivityType> getTypesByCategory(Long categoryId);
+    ActivityType getType(Long id);
 }
---
+++ src/main/java/com/example/demo/service/EmissionFactorService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+import com.example.demo.entity.EmissionFactor;
+import java.util.*;
+public interface EmissionFactorService{
+    EmissionFactor createFactor(EmissionFactor stu);
+    List<EmissionFactor> getAllFactors();
+    EmissionFactor getFactor(Long id);
+    EmissionFactor getByEmail(String email);
+}
