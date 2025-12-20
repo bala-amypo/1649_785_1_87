1e754eff7320b749872ae245d7a77f1ce1fceb38
bala-amypo <bala@amypo.in>
2025-12-20 16:31:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.beans.factory.annotation.Override;
 @Service
-public class StudentServiceImpl implements StudentService{
+public class UserServiceImpl implements UserService{
      @Autowired StudentRepo student;
      
           @Override
