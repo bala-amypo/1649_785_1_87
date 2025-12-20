0f19b89362d7db8b9b88fb6eff133605a2ebe140
bala-amypo <bala@amypo.in>
2025-12-20 13:52:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/EmissionFactor.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+public class EmissionFactor{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Double factorValue;
+    private String unit;
+    @PrePersist
+    private LocalDateTime createdAt;
+}
