b86651b5917bc7448dc268563019b4ea0293928c
bala-amypo <bala@amypo.in>
2025-12-22 09:12:41 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -21,7 +21,7 @@ public class UserServiceImpl implements UserService{
           
           @Override
           public User getUser(Long id){
-               return user.findById(id).orElseThrow(()->error.userNotFound());
+               return user.findById(id).orElseThrow(()->new ResourceNotFoundException("Not Found"));
           }
           // @Override
           // public User getByEmail(String email){
