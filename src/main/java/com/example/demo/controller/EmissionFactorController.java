9acb79061106fe41103d065ce5548f74f06ace75
bala-amypo <bala@amypo.in>
2025-12-25 08:38:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmissionFactorController.java
+++ src/main/java/com/example/demo/controller/EmissionFactorController.java
@@ -1,33 +1,23 @@
 package com.example.demo.controller;
-import java.util.*;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.demo.entity.EmissionFactor;
 import com.example.demo.service.EmissionFactorService;
-@RestController
-public class EmissionFactorController{
-    @Autowired EmissionFactorService serviceEF;
-    @PostMapping("/api/Factors/register")
-    public EmissionFactor sendData1(@RequestBody EmissionFactor stu){
-        return serviceEF.createFactor(stu);
-    }
-    @GetMapping("/api/Factors/all")
-    public List<EmissionFactor> geAllData1(){
-        return serviceEF.getAllFactors();
-    }
-    @GetMapping("/api/Factors/{id}")
-    public EmissionFactor FindVal1(@PathVariable Long id){
-        return serviceEF.getFactor(id);
-    }  
-    // @GetMapping("/api/Factors/{email}")
-    // public EmissionFactor UpdateData1(@PathVariable String email){
-    //         return serviceEF.getByEmail(email);
-    // }
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/factors")
+public class EmissionFactorController {
+
+    private final EmissionFactorService factorService;
+
+    public EmissionFactorController(EmissionFactorService factorService) {
+        this.factorService = factorService;
+    }
+
+    @GetMapping("/type/{typeId}")
+    public ResponseEntity<EmissionFactor> getFactorByType(@PathVariable Long typeId) {
+        EmissionFactor factor = factorService.getFactorByType(typeId);
+        return ResponseEntity.ok(factor);
+    }
 }
