fae4926c099a078a8a697c5a809c05b2661e38d3
bala-amypo <bala@amypo.in>
2025-12-25 08:14:23 +0000

    User commit

--- src/main/java/com/example/demo/entity/EmissionFactor.java
+++ src/main/java/com/example/demo/entity/EmissionFactor.java
@@ -1,27 +1,17 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.GenerationType;
-import lombok.Data;
+
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+
 @Entity
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-public class EmissionFactor{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+public class EmissionFactor {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
     private ActivityType activityType;
     private Double factorValue;
     private String unit;
-    
     private LocalDateTime createdAt;
-    public void PrePersist() {
-        this.createdAt = LocalDateTime.now();
-    }
+    @PrePersist
+    public void prePersist() { if(createdAt==null) createdAt=LocalDateTime.now(); }
 }
